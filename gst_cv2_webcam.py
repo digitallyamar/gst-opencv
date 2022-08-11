1bc34f9d6bcf8b4c5b364fbd09781516b2f522d4
digitallyamar <digitallyamar@gmail.com>
2022-08-11 17:15:51 +0530

    Added appsrc to udp support

--- gst_cv2_webcam.py
+++ gst_cv2_webcam.py
@@ -3,17 +3,25 @@ import cv2
 fps = 15
 frame_width = 640
 frame_height = 480
-show = True
+# Enabling show below will create openCV output window
+# Disabling it will allow us to view rtsp sream through 
+show = False
 
 webcam_app_sink = "v4l2src device=/dev/video0 ! videoconvert ! appsink"
-appsink2file = "appsrc ! videoconvert ! x264enc ! video/x-h264, profile=baseline ! matroskamux ! filesink location=webcam_stream.mkv"
+# appsink2file = "appsrc ! videoconvert ! x264enc ! video/x-h264, profile=baseline ! matroskamux ! filesink location=webcam_stream.mkv"
 
+# gst-launch cmd output window
+# Gst RTSP H264 output command:
+# gst-launch-1.0  udpsrc port=5000 ! "application/x-rtp, media=(string)video, \
+# clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! \
+# rtph264depay ! h264parse ! decodebin ! videoconvert ! autovideosink sync=false
+appsink2udp = 'appsrc ! videoconvert ! video/x-raw,width=640,height=480! x264enc ! rtph264pay ! udpsink host=127.0.0.1 port=5000'
 
 cap = cv2.VideoCapture(webcam_app_sink, cv2.CAP_GSTREAMER)
 
 
 # Create videowriter
-out = cv2.VideoWriter(appsink2file, 0, fps, (frame_width, frame_height), True)
+out = cv2.VideoWriter(appsink2udp, 0, fps, (frame_width, frame_height), True)
 
 if not cap.isOpened():
     print("Cannot capture from camera. Exiting.")
